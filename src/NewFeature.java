636be4c7bb8e0fdbce5f1d76b973e79f9db9d6c9
pratikwaghade <pratikwaghade31@gmail.com>
2025-06-19 16:29:42 +0530

    Fix error NewFeature.java

--- src/NewFeature.java
+++ src/NewFeature.java
@@ -1,5 +1,5 @@
 public class NewFeature {
     public static void main(String[] args) {
-        System.out.println(This is the new feature file added!);
+        System.out.println("This is the new feature file added!");
     }
 }
